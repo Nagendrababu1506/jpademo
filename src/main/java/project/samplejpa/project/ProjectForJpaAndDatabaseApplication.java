fdbb5d1de3b8b3bc67972d98c1b2e7fae3a8ab22
Nagendrababu1506 <43489357+Nagendrababu1506@users.noreply.github.com>
2023-10-03 10:57:28 -0500

    initial commit

---
+++ src/main/java/project/samplejpa/project/ProjectForJpaAndDatabaseApplication.java
@@ -0,0 +1,13 @@
+package project.samplejpa.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForJpaAndDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectForJpaAndDatabaseApplication.class, args);
+	}
+
+}
